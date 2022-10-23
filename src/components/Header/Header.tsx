23c82ac4bfc71f1ea99b7503ed7a3a573d6de5f3
Zheniqq <yuheni.mikhaylov@gmail.com>
2022-10-23 10:09:55 +0300

    add css to Header and Password

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,25 +4,23 @@ import {ReactComponent as LightSvg} from "assets/img/sun-line.svg";
 import {ReactComponent as DarkSvg} from "assets/img/moon-line.svg";
 import {ThemesType} from "common/types";
 import {DARK_THEME, LIGHT_THEME} from "common/constants";
-
 interface IHeader {
     theme: ThemesType,
     setTheme: (theme: ThemesType) => void
 }
-
 export const Header: FC<IHeader> = ({theme, setTheme}) => {
     const onSetThemeSVGClick = () => {
         const newTheme = theme === DARK_THEME ? LIGHT_THEME : DARK_THEME
         setTheme(newTheme)
     }
-
     return (
         <header className={style.header}>
             <h1 className={style.header__title}>Password Generator</h1>
             {
                 theme === DARK_THEME ?
                     <LightSvg className={style.header__lightSVG} onClick={onSetThemeSVGClick}/>
-                    : <DarkSvg className={style.header__darkSVG} onClick={onSetThemeSVGClick}/>}
+                    : <DarkSvg className={style.header__darkSVG} onClick={onSetThemeSVGClick}/>
+            }
 
         </header>
     );
